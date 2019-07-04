9b49aead580174bbbe9b711e23cfdd5077c95dff
gangki <leekw212@gmail.com>
2019-07-04 09:08:02 +0900

    crawling

---
+++ 01_Crawling/naver_cine_netizen_review_Green_Book.R
@@ -0,0 +1,69 @@
+# NAVER 영화('그린북') 일반인 리뷰 크롤링
+library(rvest)
+library(stringr)
+library(dplyr)
+library(openxlsx)
+
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+
+url_base <- 'https://movie.naver.com'
+start_url <- '/movie/bi/mi/point.nhn?code=171539#tab'
+url <- paste0(url_base, start_url, encoding="euc-kr")
+html <- read_html(url)
+page <- '&page='
+
+html %>%
+  html_node('iframe.ifr') %>%
+  html_attr('src') -> url2
+ifr_url <- paste0(url_base, url2) 
+html2 <- read_html(ifr_url)
+html2 %>%
+  html_node('div.score_total') %>%
+  html_node('strong.total') %>%
+  html_text('em') %>%
+  trim() -> n
+
+str_sub(n, 9, 13) -> total #숫자 추출
+total1 <- as.numeric(gsub(",", "", total))
+str(total1)
+total2 <- ceiling(total1/10) # 소수점 이하 올림처리
+
+i <- 1
+df_reviews <- data.frame(score=c(), review=c(), writer=c(), time=c())
+wb <- createWorkbook()
+for (i in 1:total2){
+  ifr_url_page <- paste0(url_base, url2, page, i) 
+  html2 <- read_html(ifr_url_page)
+  html2 %>%
+    html_node('div.score_result') %>%
+    html_nodes('li') -> lis
+  
+  score <- c()
+  review <- c()
+  writer <- c()
+  time <- c()
+  
+  for (li in lis) {
+    score <- c(score, html_node(li, '.star_score') %>% html_text('em') %>% trim())
+    li %>%
+      html_node('.score_reple') %>%
+      html_text('p') %>%
+      trim() -> tmp
+    idx <- str_locate(tmp, "\r")
+    review <- c(review, str_sub(tmp, 1, idx[1]-1))
+    tmp <- trim(str_sub(tmp, idx[1], -1))
+    idx <- str_locate(tmp, "\r")
+    writer <- c(writer, str_sub(tmp, 1, idx[1]-1))
+    tmp <- trim(str_sub(tmp, idx[1], -1))
+    idx <- str_locate(tmp, "\r")
+    time <- c(time, str_sub(tmp, 1, idx[1]-1))
+  }
+  review = data.frame(score=score, review=review, writer=writer, time=time)
+  df_reviews <- rbind.data.frame(df_reviews, review)
+}
+str(df_reviews)
+
+addWorksheet(wb, 'reviews_GreenBook')
+writeDataTable(wb, 'reviews_GreenBook', df_reviews)
+saveWorkbook(wb, file="D:/Workspace/R-project/01_Crawling/reviews.xlsx")
+
